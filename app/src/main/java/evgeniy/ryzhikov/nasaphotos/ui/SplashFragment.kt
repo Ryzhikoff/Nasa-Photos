b096afa51b86652a67396103389091c188cb5167
Evgeniy Ryzhikov <evgeniy.ryzhikov@gmail.com>
2023-12-13 14:57:57 +0700

    - create all empty fragments - create nav graph

---
+++ app/src/main/java/evgeniy/ryzhikov/nasaphotos/ui/SplashFragment.kt
@@ -0,0 +1,7 @@
+package evgeniy.ryzhikov.nasaphotos.ui
+
+import androidx.fragment.app.Fragment
+import evgeniy.ryzhikov.nasaphotos.R
+
+class SplashFragment : Fragment(R.layout.fragment_splash) {
+}
