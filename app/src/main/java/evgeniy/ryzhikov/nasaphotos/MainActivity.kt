a908a41cd82bfdea98343d96db7cd057a6bc203c
Evgeniy Ryzhikov <evgeniy.ryzhikov@gmail.com>
2023-12-13 13:48:02 +0700

    First commit

---
+++ app/src/main/java/evgeniy/ryzhikov/nasaphotos/MainActivity.kt
@@ -0,0 +1,11 @@
+package evgeniy.ryzhikov.nasaphotos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
